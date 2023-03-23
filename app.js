d6a34873ea280866639e1d382f6c0ebc1a8c49a9
Ahmed Al-Qossab <alqossaba@gmail.com>
2023-03-23 03:30:42 -0400

    lil changes

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const todoRouter = require('./routes/todoRouter');
+const subTodoRouter = require('./routes/subTodoRouter');
+const heatMapRouter = require('./routes/heatMapRouter');
 
 const app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/todos', todoRouter);
+app.use('/subTodos', subTodoRouter);
+app.use('/heatMap', heatMapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
