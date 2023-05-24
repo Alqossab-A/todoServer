c99a2dcb48607e789bce1a9121c6ce2cdc8cfdf9
Ahmed Al-Qossab <alqossaba@gmail.com>
2023-05-24 01:53:39 -0400

    user router

--- app.js
+++ app.js
@@ -36,43 +36,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser('12345-6789-09876-54321'));
 
-app.use(session({
-    name: 'session-id',
-    secret: '12345-6789-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
+const fileStoreOptions = {
+    retries: 0,
+};
+
+app.use(
+    session({
+        name: 'session-id',
+        secret: '12345-6789-09876-54321',
+        saveUninitialized: false,
+        resave: false,
+        store: new FileStore(fileStoreOptions),
+    })
+);
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
 
 function auth(req, res, next) {
     console.log(req.session);
-    
+
     if (!req.session.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error('Your are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
-            .toString()
-            .split(':');
-        const user = auth[0];
-        const pass = auth[1];
-
-        if (user === 'admin' && pass === 'password') {
-            req.session.user = 'admin'
-            return next(); // authorized
-        } else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+        const err = new Error('Your are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.session.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
@@ -83,10 +72,9 @@ function auth(req, res, next) {
 }
 
 app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', userRouter);
 app.use('/todos', todoRouter);
 app.use('/subTodos', subTodoRouter);
 app.use('/extraTodos', extraTodoRouter);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,27 +10,91 @@ userRouter.get('/', function (req, res, next) {
 
 userRouter.post('/signup', (req, res, next) => {
     User.findOne({ username: req.body.username })
-    .then((user) => {
-        if (user) {
-            const err = new Error(
-                `Username ${req.body.username} is already taken :(`
-            );
-            err.status = 403;
+        .then((user) => {
+            if (user) {
+                const err = new Error(
+                    `Username ${req.body.username} is already taken :(`
+                );
+                err.status = 403;
+                return next(err);
+            } else {
+                User.create({
+                    username: req.body.username,
+                    password: req.body.password,
+                })
+                    .then((user) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({
+                            status: 'Successfully Sign-Up',
+                            user: user,
+                        });
+                    })
+                    .catch((err) => next(err));
+            }
+        })
+        .catch((err) => next(err));
+});
+
+userRouter.post('/login', (req, res, next) => {
+    if (!req.session.user) {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            const err = new Error('Your are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
             return next(err);
-        } else {
-            User.create({
-                username: req.body.username,
-                password: req.body.password,
-            })
-            .then((user) => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json({ status: 'Successfully Sign-Up', user: user });
-            })
-            .catch(err => next(err));
         }
-    })
-    .catch(err => next(err));
+
+        const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
+            .toString()
+            .split(':');
+        const username = auth[0];
+        const password = auth[1];
+
+        User.findOne({ username: username })
+            .then((user) => {
+                if (!user) {
+                    const err = new Error(`User ${username} doesnt exist :/`);
+                    err.status = 401;
+                    return next(err);
+                } else if (user.password !== password) {
+                    const err = new Error(`Incorrect password :/`);
+                    err.status = 401;
+                    return next(err);
+                } else if (
+                    user.username === username &&
+                    user.password === password
+                ) {
+                    req.session.user = 'authenticated';
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/plain');
+                    res.end('You have Logged-In :)');
+                }
+            })
+            .catch((err) => next(err));
+    } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('You are already logged in :)');
+    }
+});
+
+userRouter.get('/logout', (req, res, next) => {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.clearCookie('session-id');
+                res.redirect('/');
+            }
+        });
+    } else {
+        const err = new Error('You are not Logged in! sussy');
+        err.status = 401;
+        return next(err);
+    }
 });
 
 module.exports = userRouter;
